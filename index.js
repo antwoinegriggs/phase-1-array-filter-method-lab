836e5609798d2662657e18c410b5b7a5aefaa72f
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-03-28 23:06:57 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const findMatching = function (driverList, name) {
+  let findDrivers = driverList.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+  return findDrivers;
+};
+
+const fuzzyMatch = function (driverList, name) {
+  let findDrivers = driverList.filter((driver) => driver[0] === name[0]);
+  return findDrivers;
+};
+
+const matchName = function (driverList, name) {
+  let findDrivers = driverList.filter((driver) => driver.name === name);
+  return findDrivers;
+};
